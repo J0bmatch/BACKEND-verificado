e25d6d9e4a1ef139bda56c7dc5eb005ef7d46a5b
JobMatch <J0bMatch@outlook.com>
2024-10-16 17:18:56 -0300

    Add files via upload

---
+++ src/pages/empresa/receberDadosE/receberDadosE.controller.ts
@@ -0,0 +1,3 @@
+//receber os dados controller
+import { Endereco } from '../../ambos/compartilhado.entity';
+import { SegmentoAtuacao } from '../segmentoAtuacao.entity';
---
+++ src/pages/empresa/receberDadosE/receberDadosE.service.ts
@@ -0,0 +1,3 @@
+//receber os dados service
+import { Endereco } from '../../ambos/compartilhado.entity';
+import { SegmentoAtuacao } from '../segmentoAtuacao.entity';
